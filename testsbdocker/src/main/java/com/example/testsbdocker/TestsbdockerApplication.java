ffbcda4c3e7fca3d138a9bf9fdeb0df4104a980b
scalargitid <pulkit.sharma.2406@gmail.com>
2025-10-15 22:45:02 +0530

    adding basic jenkins

---
+++ testsbdocker/src/main/java/com/example/testsbdocker/TestsbdockerApplication.java
@@ -0,0 +1,13 @@
+package com.example.testsbdocker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestsbdockerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestsbdockerApplication.class, args);
+    }
+
+}
